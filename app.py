ddda62e1234edb30e46a925147540b74b395f552
lee-edu2026 <1014379223@qq.com>
2026-06-10 18:08:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ for message in st.session_state.messages:
 # --- 4. 互动逻辑 ---
 col1, col2 = st.columns([1, 12])
 with col1:
-    pass
+    extensions.voice_toggle()
 with col2:
     prompt = st.chat_input("在这里输入你的想法...")
 
